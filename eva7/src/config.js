52642c8021cfa206552e958c25e3cbbb82966399
Claudio Cataldo <claudiox43@gmail.com>
2026-06-28 11:25:00 -0400

    Fix: Add default DB credentials in config.js

--- eva7/src/config.js
+++ eva7/src/config.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const DB_CONFIG = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_DATABASE || 'eva7',
 };
 
 export const PORT = process.env.PORT || 4000;
